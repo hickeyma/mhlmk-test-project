185668120b1b46ff6941b359c100b43924a6ab9f
Martin Hickey <martin.hickey@ie.ibm.com>
2024-11-08 14:18:52 +0000

    Update after formatting
    Signed-off-by: Martin Hickey <martin.hickey@ie.ibm.com>

--- test_project/main.py
+++ test_project/main.py
@@ -1,8 +1,17 @@
-from dog import dog
+"""
+Entry point to run the package
+"""
+
+from test_project.dog import dog
+
 
 def main():
-    myDog = dog.Dog()
-    print(f'{myDog.what_am_i()}')
+    """
+    main function
+    """
+    my_dog = dog.Dog()
+    print(f"{my_dog.what_am_i()}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- tests/test_dog.py
+++ tests/test_dog.py
@@ -1,16 +1,28 @@
+"""
+Test Dog class
+"""
+
 import unittest
 
 from test_project.dog import dog
 
+
 class TestDog(unittest.TestCase):
+    """
+    Dog test class
+    """
 
     def setUp(self):
         self.dog = dog.Dog()
 
     def test_what_am_i(self):
+        """
+        testing wham_am_i() nfunction
+        """
         expected_output = "I am a dog first but also a mammal"
         actual_output = self.dog.what_am_i()
         self.assertEqual(expected_output, actual_output)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
